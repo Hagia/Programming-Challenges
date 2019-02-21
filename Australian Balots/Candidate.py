5fb8b8f3cfbc75aeb3946cf38d920065b5be86be
Hagia <36092636+Hagia@users.noreply.github.com>
2019-02-21 08:55:06 -0500

    Create Candidate.py

---
+++ Australian Balots/Candidate.py
@@ -0,0 +1,16 @@
+class candidate:
+  def _init_(self, name):
+    self.name = name
+    self.count = 0
+  
+  def increaseCount(self)
+    self.count +=1
+    
+  def getCount(self)
+    return count
+    
+  def getName(self)
+    return self.name
+  
+  def print(self)
+    print(self.name + " " + self.count)
