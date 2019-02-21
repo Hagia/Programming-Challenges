9ecb8dd870839feb2d3d0f7487c3a35a6181784d
Hagia <36092636+Hagia@users.noreply.github.com>
2019-02-21 07:36:46 -0500

    Update DataLoad.py

--- Australian Balots/DataLoad.py
+++ Australian Balots/DataLoad.py
@@ -1,26 +1,41 @@
+"""Taken from https://uva.onlinejudge.org/"""
+#Method for reading input
 def dataload():
-    data = open("Data.txt", "r")  
+    #Plain data file
+    data = open("Data.txt", "r")
+    
+    #Used data structures
     candidateSet = {}
-    dataSet = {}
+    balotSet = {}
+    
     cases = int(data.readline())
     i = 0
     while(i < cases):
         candidates = int(data.readline())
         j = 0
+        
+        #Fill candidates set
         while(j < candidates):
             candidateSet[j] = data.readline()
-            dataSet[j] = list()
+            balotSet[j] = list()
             j +=1
         j = 0
         balot = data.readline()
+        
+        #Fill balots set
         while(balot != '-'):
             while(j < candidates):
-                dataSet[j].append(balot.split()[j])
+                balotSet[j].append(balot.split()[j])
                 j+=1
             j = 0
             balot = data.readline()
         i +=1 
+        
+        #Balots set to balot queue
+        balotsQueue = list()
+        for x in range(0,candidates):
+            balotsQueue = balotsQueue + balotSet[x]
     print(candidateSet)
-    print(dataSet)
+    print(balotSet)
 
 dataload()
